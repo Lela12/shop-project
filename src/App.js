3e138ffaf934e1a5c4425b2195572461fc4f28d3
Lela12 <dbcocotn4@hanmail.net>
2022-01-17 12:44:01 +0900

    [Finish] 쇼핑몰 프로젝트

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Navbar, Nav, NavDropdown, Jumbotron, Button } from "react-bootstrap";
 import "./App.css";
 import Data from "./data.js";
 import Detail from "./Detail";
+import axios from "axios";
 
 import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   let [dessert, dessert변경] = useState(Data);
+  let [loading, loading변경] = useState(true);
+  let [재고, 재고변경] = useState([10, 11, 12]);
 
   return (
     <div className="App">
@@ -17,11 +20,11 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link>
-              <Link to="/">Home</Link>
+            <Nav.Link as={Link} to="/">
+              Home
             </Nav.Link>
-            <Nav.Link>
-              <Link to="/detail">Detail</Link>
+            <Nav.Link as={Link} to="/detail">
+              Detail
             </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -56,10 +59,38 @@ function App() {
                 return <Card dessert={a} i={i} key={i} />;
               })}
             </div>
+
+            {loading === true ? (
+              <div>
+                <p>로딩중입니다~</p>
+              </div>
+            ) : null}
+
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                loading변경(true);
+                axios.post("서버URL", { id: "codingapple", pw: 1234 });
+
+                axios
+                  .get("http://codingapple1.github.io/shop/data2.json")
+                  .then((result) => {
+                    loading변경(false);
+                    console.log(result.data);
+                    dessert변경([...dessert, ...result.data]);
+                  })
+                  .catch(() => {
+                    loading변경(false);
+                    console.log("실패");
+                  });
+              }}
+            >
+              더보기
+            </button>
           </div>
         </Route>
         <Route path="/detail/:id">
-          <Detail dessert={dessert} />
+          <Detail dessert={dessert} 재고={재고} 재고변경={재고변경} />
         </Route>
 
         <Route path="/:id">
--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
+import "./Detail.scss";
 
 let 박스 = styled.div`
   padding: 20px;
@@ -13,6 +15,21 @@ let 제목 = styled.h4`
 `;
 
 function Detail(props) {
+  let [alert창, alert창변경] = useState(true);
+  let [inputData, inputData변경] = useState("");
+
+  useEffect(() => {
+    axios.get();
+
+    let 타이머 = setTimeout(() => {
+      alert창변경(false);
+    }, 2000);
+    console.log("안녕");
+    return () => {
+      clearTimeout(타이머);
+    };
+  }, []);
+
   let { id } = useParams();
   let history = useHistory();
 
@@ -23,8 +40,22 @@ function Detail(props) {
   return (
     <div className="container">
       <박스>
-        <제목 색상="red">Detail</제목>
+        <제목 className="red">Detail</제목>
       </박스>
+
+      {inputData}
+      <input
+        onChange={(e) => {
+          inputData변경(e.target.value);
+        }}
+      />
+
+      {alert창 === true ? (
+        <div className="my-alert2">
+          <p>재고가 얼마 남지 않았습니다</p>
+        </div>
+      ) : null}
+
       <div className="row">
         <div className="col-md-6">
           <img src={"/assests/dessert1.jpg"} alt="dessert" width="100%" />
@@ -33,7 +64,15 @@ function Detail(props) {
           <h4 className="pt-5">{찾는상품?.title}</h4>
           <p>{찾는상품?.content}</p>
           <p>{찾는상품?.price}</p>
-          <button className="btn btn-danger">주문하기</button>
+          <Info 재고={props.재고} />
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              props.재고변경([9, 11, 12]);
+            }}
+          >
+            주문하기
+          </button>
           &nbsp;
           <button
             className="btn btn-danger"
@@ -48,4 +87,9 @@ function Detail(props) {
     </div>
   );
 }
+
+function Info(props) {
+  return <p>재고 : {props.재고[0]}</p>;
+}
+
 export default Detail;
